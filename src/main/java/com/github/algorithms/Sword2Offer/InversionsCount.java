482dda80074cd814561fd5fba533b4b4777f961c
zsx <zsx@ali-1c36bbeda387.local>
2018-09-02 17:20:28 +0800

    逆序数新知

--- src/main/java/com/github/algorithms/Sword2Offer/InversionsCount.java
+++ src/main/java/com/github/algorithms/Sword2Offer/InversionsCount.java
@@ -11,10 +11,18 @@ package com.github.algorithms.Sword2Offer;
  * <p>
  * ----update2018-08-12----
  * 发现还是不会，说名之前就没理解
+ * <p>
+ * ------update2018-09-02-----
+ * 线段树思路：http://www.ahathinking.com/archives/135.html
+ * https://www.cnblogs.com/zhenglw/p/9507884.html
  */
 public class InversionsCount {
-    public static void main(String[] args) {
+    int count = 0;
 
+    public static void main(String[] args) {
+        InversionsCount ic = new InversionsCount();
+        int[] nums = {7, 5, 6, 4}; //expect:5
+        ic.sovleMain(nums);
     }
 
     /**
@@ -22,10 +30,60 @@ public class InversionsCount {
      * 如果元素逆序就统计加一即可!
      * <p>
      * O(n lg n)
+     */
+    public void sovleMain(int[] nums) {
+        int[] another = new int[nums.length];
+        solve(nums, 0, nums.length - 1, another);
+    }
+
+    public void solve(int[] nums, int first, int last, int[] tmp) {
+        if (first < last) {
+            int mid = (first + last) / 2;
+            solve(nums, first, mid, tmp);// 左边有序
+            solve(nums, mid + 1, last, tmp); // 右边有序
+            mergeArray(nums, first, mid, last, tmp); // 合并
+
+        }
+    }
+
+    /**
+     * 将两个有序序列合并！
      *
      * @param nums
+     * @param first
+     * @param mid
+     * @param last
+     * @param temp
      */
-    public void solve(int[] nums) {
+    public void mergeArray(int[] nums, int first, int mid, int last, int[] temp) {
+        int i = first, j = mid + 1; // 两个游标！
+        int k = 0;
+        while (i <= mid && j <= last) {
+            if (nums[i] > nums[j]) {
+                // 左数组比右数组大
+                temp[k++] = nums[j++];
+                // 因为如果a[i]此时比右数组的当前元素a[j]大，
+                // 那么左数组中a[i]后面的元素就都比a[j]大
+                // 【因为数组此时是有序数组】
+                count += mid - i + 1;
+            } else {
+                temp[k++] = nums[i++];
+            }
+        }
 
+        while (i <= mid) {
+            temp[k++] = nums[i++];
+        }
+        while (j <= last) {
+            temp[k++] = nums[j++];
+        }
+        for (i = 0; i < k; i++)
+            nums[first + i] = temp[i];
+
+        System.out.println("res : " + count);
     }
+
+    /**
+     * http://acm.xidian.edu.cn/problemset.php?page=1
+     */
 }
